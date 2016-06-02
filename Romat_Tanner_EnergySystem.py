df4d74e8d1942d077977b2e79073538729acb5ff
SquareRooted <luongo-best-1@hotmail.com>
2016-06-02 11:06:18 -0400

    Update Romat_Tanner_EnergySystem.py

--- Romat_Tanner_EnergySystem.py
+++ Romat_Tanner_EnergySystem.py
@@ -3,9 +3,13 @@ energy = myFile.readlines()
 energy = ', '.join(energy)
 task_completed = True # Get checkbox value from gui
 points = 5 # Get point value from gui
+action_taken = False # If player fights, make variable true
 
 if (task_completed == True):
     energy = int(energy) + points
+    
+if (action_taken == True):
+    energy = int(energy) - 5
 
 myFile = open("energyfile.txt", "w")    
 print energy
