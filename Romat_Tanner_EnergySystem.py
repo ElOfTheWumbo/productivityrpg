5f255a089bf093f313f17be9a51ecc5b9947da3f
SquareRooted <luongo-best-1@hotmail.com>
2016-06-06 14:00:29 -0400

    Update Romat_Tanner_EnergySystem.py

--- Romat_Tanner_EnergySystem.py
+++ Romat_Tanner_EnergySystem.py
@@ -1,11 +1,13 @@
-import productivityrpg.py
-
 myFile = open("energyfile.txt", "r")
 energy = myFile.readlines()
 energy = ', '.join(energy)
 task_completed = False # Get checkbox value from gui
 points = 5 # Get point value for completion from gui
+button_clicked = True # Get clicked value from gui
+button2_clicked = True # Get clicked value from gui
+button3_clicked = True # Get clicked value from gui
 
+# Gives a certain amount of points based on task completed
 if (task_completed == True):
     energy = int(energy) + points
  
@@ -13,7 +15,12 @@ if (task_completed == True):
 if player_health <= 0 or enemy_dead == True:
     energy = int(energy) - 1
     
-
+if button_clicked == True or button2_clicked == True or button3_clicked == True:
+    energy = int(energy) - 1
+    button_clicked = False
+    button2_clicked = False
+    button3_clicked = False   
+       
 myFile = open("energyfile.txt", "w")    
 print energy
 myFile.seek(0)
