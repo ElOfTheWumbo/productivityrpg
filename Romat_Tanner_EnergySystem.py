276866c3017a659fdeca39d6bea6e7b9a91efd7a
SquareRooted <luongo-best-1@hotmail.com>
2016-06-06 13:51:12 -0400

    Update Romat_Tanner_EnergySystem.py

--- Romat_Tanner_EnergySystem.py
+++ Romat_Tanner_EnergySystem.py
@@ -1,15 +1,18 @@
+import productivityrpg.py
+
 myFile = open("energyfile.txt", "r")
 energy = myFile.readlines()
 energy = ', '.join(energy)
-task_completed = True # Get checkbox value from gui
-points = 5 # Get point value from gui
-action_taken = False # If player fights, make variable true
+task_completed = False # Get checkbox value from gui
+points = 5 # Get point value for completion from gui
 
 if (task_completed == True):
     energy = int(energy) + points
+ 
+# Takes away an energy point is the player or enemy has been defeated    
+if player_health <= 0 or enemy_dead == True:
+    energy = int(energy) - 1
     
-if (action_taken == True):
-    energy = int(energy) - 5
 
 myFile = open("energyfile.txt", "w")    
 print energy
